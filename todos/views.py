7ecac6d797042043a9c35f5a2477cfd85c3933b7
Jaishree2310 <jaishrees23102001@gmail.com>
2024-08-01 00:29:00 +0530

    feat: host with proxy pokeapi and search pokemon via name

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from .models import Todo
-from django.http import HttpResponseRedirect
+from django.http import JsonResponse
+import requests
 
 class IndexView(generic.ListView):
     template_name = 'todos/index.html'
@@ -33,3 +34,16 @@ def update(request, todo_id):
 
     todo.save()
     return redirect('todos:index')
+
+def pokemon(request):
+   pokemon_name = request.GET.get('name', 'pikachu')
+   response = requests.get('https://pokeapi.co/api/v2/pokemon/' + pokemon_name)
+
+
+   if response.status_code == 200:
+       data = response.json()
+
+
+       return JsonResponse(data)
+   else:
+       return JsonResponse({'error': 'Failed to fetch quote'}, status=400)
